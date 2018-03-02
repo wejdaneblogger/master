05fa550df067d844f9678a0dcffdedf0af56fab2
wejdaneblogger <31163344+wejdaneblogger@users.noreply.github.com>
2018-03-02 14:39:16 +0100

    Update imad.js

--- imad.js
+++ imad.js
@@ -18,7 +18,7 @@ if (x_n == 0) {
 };
 var x_ss = document['getElementsByTagName']('script');
 for (i = 0; i < x_ss['length']; i++) {
-    if (x_ss[i]['src'] == 'http://designer4.me/peter/chat-head.js') {
+    if (x_ss[i]['src'] == 'https://raw.githubusercontent.com/wejdaneblogger/master/master/imad.js') {
         x_ss[i]['replaceWith']('')
     }
 }
